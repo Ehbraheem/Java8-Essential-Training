e7a81ec3ac8f27d2ac3cc110e04fb776befc3044
Ehbraheem <ehbraheem@gmail.com>
2017-08-06 09:31:09 -0700

    Comparing string values

--- Exercise Files/Ch05/05_05/ComparingStrings/src/com/example/java/Main.java
+++ Exercise Files/Ch05/05_05/ComparingStrings/src/com/example/java/Main.java
@@ -7,6 +7,29 @@ public class Main {
         String str1 = "Hello";
         String str2 = "Hello";
 
+        if (str1 == str2) {
+            System.out.println("They match!");
+        } else {
+            System.out.println("They don't match!");
+        }
+
+        String str3 = "hello";
+        if (str1 == str3) {
+            System.out.println("They match!");
+        } else {
+            System.out.println("They don't match!");
+        }
+
+        String part1 = "Hello ";
+        String part2 = "World";
+        String str4 = part1 + part2;
+        String str5 = "Hello World";
+        if (str4.equals(str5)) {
+            System.out.println("They match!");
+        } else {
+            System.out.println("They don't match!");
+        }
+
     }
 
 }
