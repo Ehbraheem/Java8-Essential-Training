cbe805a415f72d686d20567966a27bb74decdf44
Ehbraheem <ehbraheem@gmail.com>
2017-08-06 00:00:04 -0700

    Using mathematical operators and the Math class

--- Exercise Files/Ch04/04_05/Math/src/com/example/java/Main.java
+++ Exercise Files/Ch04/04_05/Math/src/com/example/java/Main.java
@@ -7,6 +7,27 @@ public class Main {
         int intValue1 = 56;
         int intValue2 = 42;
 
+        int result1 = intValue1 + intValue2;
+        System.out.println("Adiition: " + result1);
+
+        int result2 = intValue1 - intValue2;
+        System.out.println("Subtraction: " + result2);
+
+        int result3 = intValue1 * intValue2;
+        System.out.println("Multiplication: " + result3);
+
+        double result4 = (double) intValue1 / intValue2;
+        System.out.println("Division: " + result4);
+
+        double result5 = (double) intValue1 % intValue2;
+        System.out.println("Remainder: " + result5);
+
+        double doubleValue = -3.99999;
+        long rounded = Math.round(doubleValue);
+        System.out.println("Rounded: " + rounded);
+
+        double absValue = Math.abs(doubleValue);
+        System.out.println("Absolute: " + absValue);
     }
 
 }
