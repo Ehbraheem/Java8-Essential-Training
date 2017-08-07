3e232f36421999fc9aba7aec177863aae67d6a44
Ehbraheem <ehbraheem@gmail.com>
2017-08-07 00:26:01 -0700

    Looping through collections with iterators and for-each

--- Exercise Files/Ch08/08_05/IterateCollections/src/com/example/java/Main.java
+++ Exercise Files/Ch08/08_05/IterateCollections/src/com/example/java/Main.java
@@ -1,18 +1,13 @@
 package com.example.java;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.ListIterator;
-import java.util.Set;
-
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Ordered data");
-        ArrayList<String> list = new ArrayList<String>();
+        List<String> list = new ArrayList<String>();
         list.add("California");
         list.add("Oregon");
         list.add("Washington");
@@ -21,28 +16,46 @@ public class Main {
         System.out.println(list);
         System.out.println("");
 
-//        System.out.println("ArrayList iterator");
-//        System.out.println("");
-//
-//        System.out.println("ArrayList ForEach");
-//        System.out.println("");
-//
-//        System.out.println("Java 8 method reference");
-//        System.out.println("");
-//
-//        System.out.println("Unordered data");
-//        HashMap<String, String> map = new HashMap<String, String>();
-//        map.put("California", "Sacramento");
-//        map.put("Oregon", "Salem");
-//        map.put("Washington", "Olympia");
-//        System.out.println(map);
-//        System.out.println("");
-//
-//        System.out.println("HashMap Iterator");
-//        System.out.println("");
-//
-//        System.out.println("HashMap ForEach");
-//        System.out.println("");
+        System.out.println("ArrayList iterator");
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            String value = iterator.next();
+            System.out.println(value);
+        }
+        System.out.println("");
+
+        System.out.println("ArrayList ForEach");
+        for (String value : list) {
+            System.out.println(value);
+        }
+        System.out.println("");
+
+        System.out.println("Java 8 method reference");
+        list.forEach(System.out::println);
+        System.out.println("");
+
+        System.out.println("Unordered data");
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("California", "Sacramento");
+        map.put("Oregon", "Salem");
+        map.put("Washington", "Olympia");
+        System.out.println(map);
+        System.out.println("");
+
+        System.out.println("HashMap Iterator");
+        Set<String> keys = map.keySet();
+        Iterator<String> iterator1 = keys.iterator();
+        while (iterator1.hasNext()) {
+            String key = iterator1.next();
+            System.out.println("The capitol of " + key + " is " + map.get(key));
+        }
+        System.out.println("");
+
+        System.out.println("HashMap ForEach");
+        for (String key : keys) {
+            System.out.println("The capitol of " + key + " is " + map.get(key));
+        }
+        System.out.println("");
 
     }
 
